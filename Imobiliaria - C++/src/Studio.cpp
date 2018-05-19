db4d4893b466308cf4e4a04a5fe4603ab6c32d4f
Brunogomes97 <36930139+Brunogomes97@users.noreply.github.com>
2018-05-19 11:35:33 -0300

    V0.9
    Flat e Studio Adicionados, faltando função delete.

--- Imobiliaria - C++/src/Studio.cpp
+++ Imobiliaria - C++/src/Studio.cpp
@@ -17,30 +17,65 @@ string Studio::getdescricao(){
 
     stringstream ss;
     ss<<"********************************************\n"
-      <<"STUDIO                                       \n"
-      <<"**********Informacoes Especificas***********\n"
       <<"Area do Studio: "<<areaST<<"m2"<<          "\n"
-      <<"Quant. de Quartos: "<<quartosST<<"m2"<<    "\n"
-      <<"Referencia: "<<posicaoST<<"m2"<<           "\n"
+      <<"Quant. de Quartos: "<<quartosST<<          "\n"
+      <<"Referencia: "<<posicaoST<<                 "\n"
       <<"********************************************\n";
 
+    boolST();
 
     return ss.str();
 
 }
+
+void Studio::boolST(){
+    cout<<"********************************************\n";
+    cout<<"STUDIO                                      \n";
+    cout<<"**********Informacoes Especificas***********\n";
+    cout<<("POSSUI:\n");
+    if(arST == 1)
+        cout<<("Ar Condicionado   \n");
+    if(internet_tvST == 1)
+        cout<<("Internet e TV     \n");
+    if(lavanderiaST == 1)
+        cout<<("Lavanderia e Servicos \n");
+    if(limpezaST == 1)
+        cout<<("Serv. de limpeza  \n");
+    if(recepcaoST == 1)
+        cout<<("Recepcao 24h      \n");
+    if(piscinaST == 1)
+        cout<<("Piscina           \n");
+    if(saunaST == 1)
+        cout<<("Sauna             \n");
+    if(ginasticaST == 1)
+        cout<<("Sala de Ginast.   \n");
+    cout<<"********************************************\n";
+
+
+
+
+
+
+
+
+
+}
+
+
 void Studio::cadEspecifico(){
 
     int x;
     do{
         cout<<"*********************************************************\n";
-        cout<<"Insira as informacoes especificas da casa:               \n";
+        cout<<"Insira as informacoes especificas do Studio:             \n";
         cout<<"*********************************************************\n";
         cout<<"1-Informe a area ocupada pelo Studio(em metro quadrados):\n";
         cin>>areaST;
         cout<<"2-Informe o numero de quartos do Studio:                 \n";
         cin>>quartosST;
         cout<<"3-Informe alguma referencia(posicao):                    \n";
-        cin>>posicaoST;
+        setbuf(stdin,NULL);
+        getline(cin,posicaoST);
         cout<<"4-Numeros de vagas na garagem?:                          \n";
         cin>>vagasST;
         cout<<"5-O Studio possui ar condicionado?(Sim - 1/Nao - 0):     \n";
@@ -59,7 +94,7 @@ void Studio::cadEspecifico(){
         cin>>saunaST;
         cout<<"12-O Studio possui sala de ginastica?(Sim - 1/Nao - 0):  \n";
         cin>>ginasticaST;
-        cout<<"Deseja salvar as alteracoes de CASA?(1-SIM ou Qualquer numero para Repetir o Preenchimento)\n";
+        cout<<"Deseja salvar as alteracoes de STUDIO?(1-SIM ou Qualquer numero para Repetir o Preenchimento)\n";
         cin>>x;
 
     system("cls");
